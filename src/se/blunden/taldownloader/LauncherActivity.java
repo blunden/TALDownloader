cce730b7588f6ea20ef07d9e72786761c694f97f
blunden <blunden2@gmail.com>
2014-11-30 12:05:04 +0100

    Check the status code and show some basic error messages

--- src/se/blunden/taldownloader/LauncherActivity.java
+++ src/se/blunden/taldownloader/LauncherActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LauncherActivity extends ActionBarActivity {
 	private static final String TAG = "TALDownloader";
@@ -26,18 +27,41 @@ public class LauncherActivity extends ActionBarActivity {
 
         downloadButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
-                	// TODO: Display error to the user if invalid
-                	// ie. check the return value and create alert dialog
+
                 	String episode = episodeInputField.getText().toString().trim();
-                	downloader.download(episode);
+                	int status = downloader.download(episode);
                 	
                 	// Clear the input field
                 	episodeInputField.setText("");
                 	
+                	checkStatus(status);
+                	
                 	Log.d(TAG, "input: " + episode);
                 }
             });
-        
-        //TODO: Check download status
+	}
+	
+	private void checkStatus(int status) {
+		switch(status) {
+			case StatusCode.DIALOG_ERROR_CONNECTION_ID:
+				Toast.makeText(this, getString(R.string.error_connection_message), Toast.LENGTH_LONG).show();
+				break;
+			
+			case StatusCode.DIALOG_ERROR_EPISODE_ID:
+				Toast.makeText(this, getString(R.string.error_episode_title), Toast.LENGTH_LONG).show();
+				break;
+			
+			case StatusCode.DIALOG_ERROR_STORAGE_ID:
+				Toast.makeText(this, getString(R.string.error_storage_message), Toast.LENGTH_LONG).show();
+				break;
+				
+			case StatusCode.SUCCESS:
+				// All is well, nothing to do
+				break;
+			
+			default:
+				// Assume all is well
+				break;
+		}
 	}
 }
